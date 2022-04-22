2c70e773cb713a81c08667d9657843454e380e49
slwu89 <slwu89@users.noreply.github.com>
2022-04-22 09:23:55 -0700

    short ex

--- test/SIR-age.jl
+++ test/SIR-age.jl
@@ -21,28 +21,6 @@ end
 
 @acset_type AgeSIR(ThAgeSIR)
 
-# setup a model...
-sir_index = vcat(fill.(["S", "I", "R"],[3, 2, 1])...)
-shuffle!(sir_index)
-
-state = AgeSIR{Int64}()
-add_parts!(state, :S, sum(sir_index .== "S"))
-add_parts!(state, :I, sum(sir_index .== "I"))
-add_parts!(state, :R, sum(sir_index .== "R"))
-
-N = 6
-add_parts!(state, :Agent, N)
-add_parts!(state, :Age, N)
-
-set_subpart!(state, 1:nparts(state, :S), :s, findall(sir_index .== "S"))
-set_subpart!(state, 1:nparts(state, :I), :i, findall(sir_index .== "I"))
-set_subpart!(state, 1:nparts(state, :R), :r, findall(sir_index .== "R"))
-
-set_subpart!(state, 1:N, :age, 1:N)
-
-ages = sample(1:80, N)
-set_subpart!(state, 1:N, :agevalue, ages)
-
 # infection rules
 I = @acset AgeSIR{Int64} begin Agent=2; I=1; i=[2] end # need 2 agents otherwise have dangling edges
 I2 = @acset AgeSIR{Int64} begin Agent=2; I=2; i=[1,2] end
@@ -59,32 +37,42 @@ A1 = @acset AgeSIR{Int64} begin Agent=1 end
 L_recover = ACSetTransformation(A1, I1; Agent = [1])
 R_recover = ACSetTransformation(A1, R1; Agent = [1])
 
-# test recovery
-is_natural(L_recover)
-is_natural(R_recover)
+# setup a model with 6 persons
+N = 6
+sir_index = vcat(fill.(["S", "I", "R"],[3, 2, 1])...)
+shuffle!(sir_index)
+ages = [50,23,51,49,16,19]
 
-rec_ind = homomorphisms(I1, state)
-can_pushout_complement(L_recover, rec_ind[1])
+state = AgeSIR{Int64}()
+add_parts!(state, :S, sum(sir_index .== "S"))
+add_parts!(state, :I, sum(sir_index .== "I"))
+add_parts!(state, :R, sum(sir_index .== "R"))
 
-state_new = rewrite_match(L_recover, R_recover, rec_ind[2])
+add_parts!(state, :Agent, N)
+add_parts!(state, :Age, N)
+
+set_subpart!(state, 1:nparts(state, :S), :s, findall(sir_index .== "S"))
+set_subpart!(state, 1:nparts(state, :I), :i, findall(sir_index .== "I"))
+set_subpart!(state, 1:nparts(state, :R), :r, findall(sir_index .== "R"))
+
+set_subpart!(state, 1:N, :age, 1:N)
+# set_subpart!(state, 1:N, :age, collect(reverse(1:N)))
+
+set_subpart!(state, 1:N, :agevalue, ages)
+# set_subpart!(state, 1:N, :agevalue, reverse(ages))
 
 # test infection
 # find matches
-inf_pairs = homomorphisms(SI, state)
-
-# get the ages of the S and I persons involved in each matching
-
-# ages of S
-vcat([state[collect(x[:S]), [:s, :age, :agevalue]] for x in inf_pairs]...)
-
-# ages of I
-vcat([state[collect(x[:I]), [:s, :age, :agevalue]] for x in inf_pairs]...)
+inf_pairs = homomorphisms(SI, state) 
 
 # check
-can_pushout_complement(L_infect, inf_pairs[1]) # fails b/c false
+can_pushout_complement(L_infect, inf_pairs[2])
 is_natural(L_infect)
 is_natural(R_infect)
 
 # apply the 2nd rewrite
 state_new = rewrite_match(L_infect, R_infect, inf_pairs[2])
 
+# age to ID
+[incident(state, age, [:age, :agevalue]) for age in ages]
+[incident(state_new, age, [:age, :agevalue]) for age in ages]
