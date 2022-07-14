986a9bd0d29f32a912e56fdbae0d0473ca47d4b4
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-07-13 17:37:05 -0700

    markov

--- test/SIR-markov.jl
+++ test/SIR-markov.jl
@@ -1,15 +1,6 @@
 using Catlab.CategoricalAlgebra, Catlab.Graphs, Catlab.Present, Catlab.Graphics, Catlab.Theories
 using Catlab.CategoricalAlgebra.FinCats: FinCatGraphEq
 
-using Distributions: Exponential, Geometric, cdf
-using Random: randsubseq
-using StatsBase: sample
-
-using Plots
-using ProgressBars
-
-include("utils.jl")
-
 # schema
 @present ThSIR(FreeSchema) begin
     (S,I,R,Agent)::Ob
@@ -21,13 +12,32 @@ end
 @acset_type SIR(ThSIR)
 
 # infection rules
-I = @acset SIR begin I=1 end 
-I2 = @acset SIR begin I=2 end 
-SI = @acset SIR begin S=1; I=1 end
-L_infect =  ACSetTransformation(I, SI; I=[1]) # fn from dom I to codom SI, I is the injective function mapping stuff in dom to codom
-R_infect = ACSetTransformation(I, I2; I=[1])
+inf_I = @acset SIR begin I=1 end
+inf_R = @acset SIR begin I=2 end
+inf_L = @acset SIR begin S=1; I=1 end
+inf_l =  ACSetTransformation(inf_I, inf_L; I=[1]) # fn from dom I to codom SI, I is the injective function mapping stuff in dom to codom
+inf_r = ACSetTransformation(inf_I, inf_R; I=[1])
 
 # recovery rules
-R = @acset SIR begin R=1 end
-L_recovery = ACSetTransformation(SIR(), I)
-R_recovery = ACSetTransformation(SIR(), R)
+rec_L = @acset SIR begin Agent=1; I=1; i=[1] end
+rec_R = @acset SIR begin Agent=1; R=1; r=[1] end
+rec_l = ACSetTransformation(SIR(), rec_L)
+rec_r = ACSetTransformation(SIR(), rec_R)
+
+sir = @acset SIR begin Agent=3; I=2; S=1; i=[1,2]; s=[3] end
+
+m = homomorphisms(rec_L, sir, monic = true)
+
+can_pushout_complement(rec_l, m[1])
+
+ik, kg = pushout_complement(rec_l, m[1])
+
+# shouldn't work
+rec_I1 = @acset SIR begin Agent=1 end
+rec_l = ACSetTransformation(rec_I1, rec_L; Agent=[1])
+
+m = homomorphisms(rec_L, sir, monic = true)
+
+can_pushout_complement(rec_l, m[1])
+
+ik, kg = pushout_complement(rec_l, m[1])
